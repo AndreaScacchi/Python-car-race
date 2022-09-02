6be199e5cfb00889ed527df3fd4de96c59efbe64
AndreaScacchi <andreascacchi10@gmail.com>
2022-09-02 21:54:54 +0200

    comments added and display and font setted

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ import math
 from utils import scale_image, blit_rotate_center, blit_text_center
 pygame.font.init()
 
+# Load and scale other images
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.jpg"), 0.9)
 
+# Load and scale track and finish images
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 FINISH = pygame.image.load("imgs/finish.png")
 FINISH_MASK = pygame.mask.from_surface(FINISH)
 FINISH_POSITION = (130, 250)
 
+# Load and scale the cars images
 RED_CAR = scale_image(pygame.image.load("imgs/red-car.png"), 0.55)
 GREEN_CAR = scale_image(pygame.image.load("imgs/green-car.png"), 0.55)
+
+# Set the display width and height
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing Game!")
+
+# Set the font
+MAIN_FONT = pygame.font.SysFont("comicsans", 44)
