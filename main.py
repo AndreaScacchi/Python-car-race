fd8bf15b8bba9f4148edd7f886e1cbcf51cd8529
AndreaScacchi <andreascacchi10@gmail.com>
2022-09-03 19:17:22 +0200

    while loop added

--- main.py
+++ main.py
@@ -244,3 +244,52 @@ def handle_collision(player_car, computer_car, game_info):
             player_car.reset()
             computer_car.next_level(game_info.level)
 
+run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
+
+# player car velocity
+player_car = PlayerCar(4, 4)
+
+# computer car velocity and path to follow
+computer_car = ComputerCar(2, 4, PATH)
+
+game_info = GameInfo()
+
+# while loop
+while run:
+    clock.tick(FPS)
+
+    draw(WIN, images, player_car, computer_car, game_info)
+
+    while not game_info.started:
+        blit_text_center(WIN, MAIN_FONT, f"Press any key to start level {game_info.level}!")
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                break
+
+            if event.type == pygame.KEYDOWN:
+                game_info.start_level()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            print("Game Over!")
+            break
+    
+    move_player(player_car)
+    computer_car.move()
+
+    handle_collision(player_car, computer_car, game_info)
+
+    if game_info.game_finished():
+        blit_text_center(WIN, MAIN_FONT, "You won the game!")
+        pygame.time.wait(5000)
+        game_info.reset()
+        player_car.reset()
+        computer_car.reset()
+
+pygame.quit()
+
